0abbdf7acd5e9019c034223a265990754c1ba7ea
Konstantin Kupriyanov <0864380@gmail.com>
2023-06-05 15:16:02 +0200

    the led is blinking

--- MyFirstSketch/MyFirstSketch.ino
+++ MyFirstSketch/MyFirstSketch.ino
@@ -1,6 +1,15 @@
+const int ledPin = 12;
 
 void setup() {
+    pinMode(ledPin, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
 }
+
